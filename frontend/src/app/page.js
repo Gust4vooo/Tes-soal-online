1362d3228b7e9177c7069612873f366d633629b3
fadilahmalfoy <wfadillah863@gmail.com>
2024-09-16 09:41:49 +0800

    fe buat tes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,13 @@
 import Registrasi from '@/app/Components/Registrasi';
 import Login from '@/app/Components/Login';
+import Buattes from '@/app/Components/Buattes';
 
 const Home = () => {
   return (
     <div>
       <Registrasi />
       <Login />
+      <Buattes />
     </div>
   );
 };
