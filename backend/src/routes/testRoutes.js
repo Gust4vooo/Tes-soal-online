8d6ed781ff22c10c05b545872af393f1a27d3b05
Gustavo <gusty.risandha@gmail.com>
2024-12-03 23:55:36 +0800

    Test Publish

--- backend/src/routes/testRoutes.js
+++ backend/src/routes/testRoutes.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import { createTest, getTest, testResultController, createTestController, publishTestController, getAllTests, fetchTestsByCategory, getAuthorTests } from '../controllers/testControllers.js';
+import { createTestController, publishTestController, getAllTests, fetchTestsByCategory, getAuthorTests } from '../controllers/testControllers.js';
 import { authenticateToken } from '../middleware/authMiddleware.js'; 
 
 
 const router = express.Router();
-
-router.post('/create-test', createTest);
-router.get('/get-test/:id', getTest);
-router.get('/test-result/:resultId', testResultController);
 
 router.post('/tests', createTestController);
 
@@ -19,15 +15,4 @@ router.get('/get-test', getAllTests);
 router.get('/author-tests', authenticateToken, getAuthorTests);
 
 
-export default router; // Menggunakan default export
-
-
-
-// const express = require("express");
-// const { createTest } = require("backend/src/controllers/testControllers.js");
-
-// const router = express.Router();
-
-// router.post("/create-test", createTest);
-
-// module.exports = router;
+export default router; 
